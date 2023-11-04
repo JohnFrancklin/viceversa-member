54af2d397f159f2ea2f4e04ed01365ac3b1a1e06
rakoto Norberto <rakotovao.soamampiavotra@gmail.com>
2023-11-04 21:47:00 +0300

    filter event

--- src/app/pages/home/components/event-filter/event-filter.component.ts
+++ src/app/pages/home/components/event-filter/event-filter.component.ts
@@ -6,77 +6,131 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-filter.component.scss'],
 })
 export class EventFilterComponent  implements OnInit {
+  /*************Category****************/ 
   categories = [
     {
       id: 1,
-      name: 'Festivals',
+      name: "Festivals",
     },
     {
       id: 2,
-      name: 'Spectacles',
+      name: "Spectacles",
     },
     {
       id: 3,
-      name: 'Conférences',
+      name: "Conférences",
     },
     {
       id: 4,
-      name: 'Soirées',
+      name: "Soirées",
     },
     {
       id: 5,
-      name: 'Festivals de nourriture',
+      name: "Festivals de nourriture",
     },
   ];
+  /**************************end category********************************************/ 
 
+  /********************************Date**************************************/ 
+  dates = [
+    {
+      id: 1,
+      name:"All date"
+    }, 
+    {
+      id: 2,
+      name:"This week"
+    }, 
+    {
+      id: 3,
+      name:"This weekend"
+    }, 
+    {
+      id: 4,
+      name:"November"
+    },
+    {
+      id: 5,
+      name:"December"
+    }
+  ]
+/**************************end date****************************************/ 
+/*****************************Venue**********************************************/ 
   venues = [
     {
       id: 1,
-      name: 'Carambol',
+      name: "Carambol",
     },
     {
       id: 2,
-      name: 'Luxa',
+      name: "Luxa",
     },
     {
       id: 3,
-      name: 'Sofitel',
+      name: "Sofitel",
     },
     {
       id: 4,
-      name: 'YoungViosion',
+      name: "YoungViosion",
     },
     {
       id: 5,
-      name: 'Festivals',
+      name: "Festivals",
+    },
+    {
+      id: 6,
+      name: "N'import qui",
     },
   ];
+/*********************************end venue**********************************************/ 
 
+/********************************location*****************************************************/ 
   locations = [
     {
       id: 1,
-      name: 'Nord',
+      name: "Nord",
     },
     {
       id: 2,
-      name: 'Sud',
+      name: "Sud",
     },
     {
       id: 3,
-      name: 'Centre',
+      name: "Centre",
     },
     {
       id: 4,
-      name: 'Est',
+      name: "Est",
     },
     {
       id: 5,
-      name: 'Ouest',
+      name: "Ouest",
+    },
+    {
+      id: 6,
+      name: "Partout",
     },
   ];
+/******************************end location*******************************************/ 
+
+selectedCategory = "";
+selectedDate = "";
+selectedVenue = "";
+selectedLocation = "";
 
   constructor() { }
 
   ngOnInit() {}
+
+
+  result() {
+    let object = {
+      selectedCategory: this.selectedCategory,
+      selectedDate: this.selectedDate,
+      selectedVenue: this.selectedVenue,
+      selectedLocation: this.selectedLocation
+    }; 
+    console.log("result", object);
+  }
 
 }
