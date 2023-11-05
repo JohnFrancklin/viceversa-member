b3bb6c6b5eb417013b2e83eb2b5a3070af0ebd88
John <johnfrancklin@yahoo.com>
2023-11-05 05:22:03 +0300

    correction layout desktop

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,6 +9,34 @@ import { EventFilterComponent } from './components/event-filter/event-filter.com
 })
 export class HomePage implements OnInit {
 
+  events = [
+    {
+      id: 1,
+      image: "event1.jpg"
+    },
+    {
+      id: 2,
+      image: "event2.jpg"
+    },
+    {
+      id: 3,
+      image: "event3.jpg"
+    },
+    {
+      id: 4,
+      image: "event4.jpg"
+    },
+    {
+      id: 5,
+      image: "event5.png"
+    },
+    {
+      id: 5,
+      image: "event6.jpg"
+    }
+
+  ]
+
   constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
